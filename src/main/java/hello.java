ade076b2db6d131e5653ecd6716cb3dd33151fc1
JJ LEE <jjtw8711@gmail.com>
2025-12-11 10:14:03 +0900

    진짜 저장

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Git Bash 연습중");
         System.out.println("두 번째 연습입니다!");
+        System.out.println("세 번째 연습입니다!");
     }
 
 }
